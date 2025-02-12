cd14fbf76fc8dacedbdf29f8c0c744bc7a8fc57a
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-02-12 09:53:02 +0800

    fix reject percentange

--- components/countboard-dashboard-uv.tsx
+++ components/countboard-dashboard-uv.tsx
@@ -982,19 +982,19 @@ export default function CountboardDashboardUv() {
                   <TableRow>
                     <TableCell colSpan={7} className="text-center"></TableCell>
                     <TableCell className={`text-center ${isNaN(totalRejectA / totalRejectOverall) ? '' : totalRejectA / totalRejectOverall > 0.75 ? 'text-red-600' : totalRejectA / totalRejectOverall > 0.4 ? 'text-yellow-600' : ''}`}>
-                      {isNaN(totalRejectA / totalRejectOverall) ? 0 : (totalRejectA / totalRejectOverall).toFixed(2)}%
+                      {isNaN(totalRejectA / totalRejectOverall) ? 0 : ((totalRejectA / totalRejectOverall)*100).toFixed(2)}%
                     </TableCell>
                     <TableCell className={`text-center ${isNaN(totalRejectB / totalRejectOverall) ? '' : totalRejectB / totalRejectOverall > 0.75 ? 'text-red-600' : totalRejectB / totalRejectOverall > 0.4 ? 'text-yellow-600' : ''}`}>
-                      {isNaN(totalRejectB / totalRejectOverall) ? 0 : (totalRejectB / totalRejectOverall).toFixed(2)}%
+                      {isNaN(totalRejectB / totalRejectOverall) ? 0 : ((totalRejectB / totalRejectOverall)*100).toFixed(2)}%
                     </TableCell>
                     <TableCell className={`text-center ${isNaN(totalRejectC / totalRejectOverall) ? '' : totalRejectB / totalRejectOverall > 0.75 ? 'text-red-600' : totalRejectC / totalRejectOverall > 0.4 ? 'text-yellow-600' : ''}`}>
-                      {isNaN(totalRejectC / totalRejectOverall) ? 0 : (totalRejectC / totalRejectOverall).toFixed(2)}%
+                      {isNaN(totalRejectC / totalRejectOverall) ? 0 : ((totalRejectC / totalRejectOverall)*100).toFixed(2)}%
                     </TableCell>
                     <TableCell className={`text-center ${isNaN(totalRejectD / totalRejectOverall) ? '' : totalRejectC / totalRejectOverall > 0.75 ? 'text-red-600' : totalRejectD / totalRejectOverall > 0.4 ? 'text-yellow-600' : ''}`}>
-                      {isNaN(totalRejectD / totalRejectOverall) ? 0 : (totalRejectD / totalRejectOverall).toFixed(2)}%
+                      {isNaN(totalRejectD / totalRejectOverall) ? 0 : ((totalRejectD / totalRejectOverall)*100).toFixed(2)}%
                     </TableCell>
                     <TableCell className={`text-center ${isNaN(totalRejectE / totalRejectOverall) ? '' : totalRejectE / totalRejectOverall > 0.75 ? 'text-red-600' : totalRejectE / totalRejectOverall > 0.4 ? 'text-yellow-600' : ''}`}>
-                      {isNaN(totalRejectE / totalRejectOverall) ? 0 : (totalRejectE / totalRejectOverall).toFixed(2)}%
+                      {isNaN(totalRejectE / totalRejectOverall) ? 0 : ((totalRejectE / totalRejectOverall)*100).toFixed(2)}%
                     </TableCell>
                   </TableRow>
               </TableBody>
