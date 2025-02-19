f1d98664ae6a272d033588f682c21c6333e68625
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-02-19 22:06:14 +0800

    fix: trailing slash

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/admin',
-  trailingSlash: false,
+  trailingSlash: true,
   images: {
     path: '/admin/_next/image',
   },
