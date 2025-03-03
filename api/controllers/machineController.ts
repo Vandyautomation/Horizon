b4d0ee9c2ec0c60c344ccf2f65d96b91ebb389e8
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-03-03 17:42:00 +0800

    fix

--- api/controllers/machineController.ts
+++ api/controllers/machineController.ts
@@ -1106,7 +1106,7 @@ export async function getEnergyMachineDaily(machine_name: string, date: string |
             ,COALESCE(totalgreen / NULLIF(timea, 0), 1) AS ooe
             ,COALESCE((totalgreen + totalwhite) / NULLIF(timea, 0), 0) AS oee
             , case when @machine_name = 'MT280100' then 7300 
-            when @machine_name = 'JW220004' then 11300
+            when @machine_name = 'JW220004' then 10300
             else 0
             end as budgetEnergyPerJam
         FROM IoT.dbo.mchstatustrx t
@@ -1192,7 +1192,7 @@ export async function getEnergyMachineDaily(machine_name: string, date: string |
             ,COALESCE((totalgreen + totalwhite) / NULLIF(timea, 0), 0) AS oee,
             COALESCE(totalgreen / NULLIF(timea, 0), 1) AS ooe
             , case when @machine_name = 'MT280100' then 7300 
-            when @machine_name = 'JW220004' then 11300
+            when @machine_name = 'JW220004' then 10300
             else 0
             end as budgetEnergyPerJam
         FROM IoT.dbo.mchstatustrx t
