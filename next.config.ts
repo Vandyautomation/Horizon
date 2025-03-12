58195475443fdb819a953f7c110cd5c6d33dd099
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-03-13 07:38:57 +0800

    adjust

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/admin',
+  assetPrefix: '/admin',
   output: "export",
   trailingSlash: true,
   images: {
     path: '/admin/_next/image',
+    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
