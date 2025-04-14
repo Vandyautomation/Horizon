ed913e54bf25b55727f9ade63c4c3a6a1bca83b1
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-04-14 19:20:50 +0800

    fix getSpindle shift 3

--- api/controllers/machineController.ts
+++ api/controllers/machineController.ts
@@ -193,7 +193,7 @@ export async function getSpindle(machine_id: string, date: string | null, shift:
     ELSE
     BEGIN
         SET @from = DATEADD(HOUR, 22, CAST(@date AS DATETIME)); 
-        SET @to = DATEADD(HOUR, 6, CAST(@date AS DATETIME));
+        SET @to = DATEADD(HOUR, 6, DATEADD(DAY, 1, cast(CAST(GETDATE() AS date)as datetime))); -- Goes into the next day
     END  
 
     SELECT s.SpindleSTD, d.highestCountSpindleInCurrentCycle as SpindleACT
