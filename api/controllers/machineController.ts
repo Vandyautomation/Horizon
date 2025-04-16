4840d6790736c9c6ccaccf48202ccf02e4b30acf
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-04-16 12:25:58 +0800

    update query

--- api/controllers/machineController.ts
+++ api/controllers/machineController.ts
@@ -1848,6 +1848,7 @@ export async function getEnergyMachineDaily(machine_name: string, date: string |
             COALESCE(totalgreen / NULLIF(timea, 0), 1) AS ooe
             , case when @machine_name = 'MT280100' then 7300 
             when @machine_name = 'JW220004' then 10300
+            when @machine_name = 'BR320109' then 5691
             else 0
             end as budgetEnergyPerJam
         FROM IoT.dbo.mchstatustrx t
