bf1266150ddda65a3555cb58292a95832880639d
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-06-02 13:50:15 +0700

    update

--- components/countboard-dashboard.tsx
+++ components/countboard-dashboard.tsx
@@ -758,7 +758,7 @@ const refetchStateData = () => mutate(stateDataKey);
 
 
   return (
-    <div className="p-2 space-y-2 w-full">
+    <div className="p-0 space-y-2 w-full">
       <div className="flex gap-4 justify-between items-center">
         {/* Left side - Logo and Building selection */}
         <div className="flex flex-col gap-4">
@@ -933,7 +933,7 @@ const refetchStateData = () => mutate(stateDataKey);
       ) : (
       <div className="flex gap-2 md:grid-cols-2 lg:grid-cols-4 text-center h-32 w-full mb-2">
         <Card className="p-0">
-          <CardHeader className="py-2 text-lg font-bold text-nowrap p-0 pb-2">Production Status</CardHeader>
+          <CardHeader className="py-2 text-lg font-bold text-nowrap p-0 pb-2">Production Output</CardHeader>
           <CardContent className="grid grid-cols-3 gap-4 p-2">
             
             <div>
