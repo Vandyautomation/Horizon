42dcfcbbdd0baea6a87e8d18abd6914812c716bc
Dzulfikar Ats Tsauri <dzulfikar.at@gmail.com>
2025-07-08 11:17:51 +0700

    feat: config trial & tao uv

--- components/countboard-dashboard-uv.tsx
+++ components/countboard-dashboard-uv.tsx
@@ -1132,7 +1132,7 @@ export default function CountboardDashboardUv() {
       setIsLoading(true);
       const machineStatus = selectedMachine?.machineStatus == 'TRIAL' ? 'NORMAL' : 'TRIAL';
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/machines/trial/${selectedMachine?.machineName}?machineStatus=${machineStatus}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/machines/trial/${selectedMachine?.machineName}?machineStatus=${machineStatus}&machineLocation=${selectedMachine?.locationName}&machineNumber=${selectedMachine?.machineNumber}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
       });
@@ -1159,7 +1159,7 @@ export default function CountboardDashboardUv() {
       setIsLoading(true);
       const machineStatus = selectedMachine?.machineStatus == 'TAO' ? 'NORMAL' : 'TAO';
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/machines/tao/${selectedMachine?.machineName}?machineStatus=${machineStatus}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/machines/tao/${selectedMachine?.machineName}?machineStatus=${machineStatus}&machineLocation=${selectedMachine?.locationName}&machineNumber=${selectedMachine?.machineNumber}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
       });
