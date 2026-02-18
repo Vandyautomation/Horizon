feb2959f704636b7bce95aaf545c1578e23c3a5d
VandyAutomation <vandyachmad2001@gmail.com>
2026-02-18 14:45:28 +0700

    adding next page in andon overalll(test)

---
+++ app/andon/status/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import AndonStatusDashboard from "@/components/andon-status-dashboard";
+
+export default function AndonStatusPage() {
+  return (
+    <div className="p-6">
+      <AndonStatusDashboard />
+    </div>
+  );
+}
